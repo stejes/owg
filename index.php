a43c83ff164719e15a0df42bbd683a5482a98518
StevenJ <s.jespers@telenet.be>
2017-02-22 15:01:00 +0100

    index jumbotron en items layout

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
+session_start();
 require_once 'bootstrap.php';
-$view = $twig->render("index.twig", array());
+use OWG\Weggeefwinkel\Business\ItemService;
+use OWG\Weggeefwinkel\Business\SectionService;
+use OWG\Weggeefwinkel\Business\CityService;
+
+
+
+$itemSvc = new ItemService();
+$itemList = $itemSvc->getLastItems();
+$sectionSvc = new SectionService();
+$sectionList = $sectionSvc->getAll();
+$citySvc = new CityService();
+$cityList = $citySvc->getAll();
+
+if(isset($_SESSION["username"])){
+    $username = $_SESSION["username"];
+}
+else{
+    $username="";
+}
+//print_r($itemList);
+$view = $twig->render("index.twig", array("itemList" => $itemList,"sectionList" => $sectionList, "username" => $username, "cityList" => $cityList));
 print($view);
